a837bf1d5d68d402ce0ee788f539913643ffdefb
Jos Sebastian <sebastian.l.josie@gmail.com>
2025-01-06 20:13:33 -0600

    layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
+  <div class="max-w-dvw min-h-dvh flex">
     <slot />
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div></div>
+  <div class="grow"></div>
 </template>
 
 <style></style>
