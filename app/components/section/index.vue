a3cfe710fe75b7bf12c1b20ce7fd27bad386251a
Jos Sebastian <sebastian.l.josie@gmail.com>
2025-01-09 11:45:28 -0600

    section

---
+++ app/components/section/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const { title } = defineProps<{
+  title?: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <div v-if="title">
+      {{ title }}
+    </div>
+
+    <slot />
+  </div>
+</template>
+
+<style></style>
