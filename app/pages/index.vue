dd139db85ecd9c0d1d0a71800c6de2e0f57e86b8
Jos Sebastian <sebastian.l.josie@gmail.com>
2025-01-09 11:26:18 -0600

    page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="grow"></div>
+  <div class="flex flex-col md:flex-row grow">
+    <CoreLeft />
+    <CoreRight />
+  </div>
 </template>
 
 <style></style>
