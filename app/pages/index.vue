f2546d6e4113168006bd25c7d53b9d7599ba8228
Jos Sebastian <sebastian.l.josie@gmail.com>
2025-01-28 12:18:00 -0600

    art canvas

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,9 +2,7 @@
 
 <template>
   <div class="h-full w-full overflow-auto">
-    <div class="fixed -z-20 h-full w-full">
-      <canvas class="-z-30 h-full w-full" />
-    </div>
+    <CoreArt />
 
     <div class="absolute flex flex-col gap-6 p-3 md:flex-row md:px-24 md:py-0">
       <CoreLeft class="h-full w-full md:sticky md:top-0 md:items-center" />
