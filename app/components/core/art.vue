2416785b2263a135e80b53fd7721389e73fbbf59
Jos Sebastian <sebastian.l.josie@gmail.com>
2025-01-29 20:39:47 -0600

    background

--- app/components/core/art.vue
+++ app/components/core/art.vue
@@ -1,8 +1,34 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const mouse = ref({
+  x: 0,
+  y: 0,
+});
+
+const updateMousePosition = (event: MouseEvent) => {
+  mouse.value.x = event.clientX;
+  mouse.value.y = event.clientY;
+};
+
+onMounted(() => {
+  window.addEventListener("mousemove", updateMousePosition);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("mousemove", updateMousePosition);
+});
+</script>
 
 <template>
-  <div class="fixed -z-20 h-full w-full">
-    <canvas class="-z-30 h-full w-full" />
+  <div
+    class="fixed -z-20 h-full w-full bg-gradient-to-b from-white to-indigo-300"
+  >
+    <div
+      class="absolute h-96 w-96 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white blur-3xl"
+      :style="{
+        transform: `translate(${mouse.x}px, ${mouse.y}px)`,
+        transition: 'transform 0.3s ease-out',
+      }"
+    />
   </div>
 </template>
 
