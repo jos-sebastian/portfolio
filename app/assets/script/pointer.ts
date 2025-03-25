375099750ce8242fd647b87b332c23582e4f9874
Jos Sebastian <sebastian.l.josie@gmail.com>
2025-03-25 17:32:28 -0500

    fluid

---
+++ app/assets/script/pointer.ts
@@ -0,0 +1,77 @@
+import { Color } from "./color";
+
+class Pointer {
+  public id = -1;
+  public texcoordX = 0;
+  public texcoordY = 0;
+  public prevTexcoordX = 0;
+  public prevTexcoordY = 0;
+  public delta = {
+    x: 0,
+    y: 0,
+  };
+  public down = false;
+  public moved = false;
+  public color;
+
+  constructor(colorPalette: string[], brightness: number) {
+    this.color = Color.generateColor(colorPalette, brightness);
+  }
+
+  public updatePointerDownData(
+    id: number,
+    posX: number,
+    posY: number,
+    canvas: HTMLCanvasElement,
+    colorPalette: string[],
+    brightness: number,
+  ) {
+    this.id = id;
+    this.down = true;
+    this.moved = false;
+    this.texcoordX = posX / canvas.width;
+    this.texcoordY = 1.0 - posY / canvas.height;
+    this.prevTexcoordX = this.texcoordX;
+    this.prevTexcoordY = this.texcoordY;
+    this.delta.x = 0;
+    this.delta.y = 0;
+    this.color = Color.generateColor(colorPalette, brightness);
+  }
+
+  public updatePointerMoveData(
+    posX: number,
+    posY: number,
+    canvas: HTMLCanvasElement,
+    hover: boolean,
+  ) {
+    this.prevTexcoordX = this.texcoordX;
+    this.prevTexcoordY = this.texcoordY;
+    this.texcoordX = posX / canvas.width;
+    this.texcoordY = 1.0 - posY / canvas.height;
+    this.delta.x = this.correctDeltaX(this.texcoordX - this.prevTexcoordX, canvas);
+    this.delta.y = this.correctDeltaY(this.texcoordY - this.prevTexcoordY, canvas);
+    if (hover) {
+      this.moved = Math.abs(this.delta.x) > 0 || Math.abs(this.delta.y) > 0;
+    } else {
+      this.moved = this.down;
+    }
+  }
+
+  public updatePointerUpData() {
+    this.down = false;
+  }
+
+  private correctDeltaX(delta: number, canvas: HTMLCanvasElement): number {
+    const aspectRatio = canvas.width / canvas.height;
+    if (aspectRatio < 1) delta *= aspectRatio;
+    return delta;
+  }
+
+  private correctDeltaY(delta: number, canvas: HTMLCanvasElement): number {
+    const aspectRatio = canvas.width / canvas.height;
+    if (aspectRatio > 1) delta /= aspectRatio;
+    return delta;
+  }
+}
+
+export { Pointer };
---
+++ app/assets/script/screenshot.ts
@@ -0,0 +1,70 @@
+import type { FBO } from "./types";
+
+class Screenshot {
+  public static normalizeTexture(
+    texture: Float32Array,
+    width: number,
+    height: number,
+  ): Uint8Array {
+    const result = new Uint8Array(texture.length);
+    let id = 0;
+    for (let i = height - 1; i >= 0; i--) {
+      for (let j = 0; j < width; j++) {
+        const nid = i * width * 4 + j * 4;
+        result[nid + 0] = Screenshot.clamp01(texture[id + 0]!) * 255;
+        result[nid + 1] = Screenshot.clamp01(texture[id + 1]!) * 255;
+        result[nid + 2] = Screenshot.clamp01(texture[id + 2]!) * 255;
+        result[nid + 3] = Screenshot.clamp01(texture[id + 3]!) * 255;
+        id += 4;
+      }
+    }
+    return result;
+  }
+
+  private static clamp01(input: number): number {
+    return Math.min(Math.max(input, 0), 1);
+  }
+
+  public static framebufferToTexture(target: FBO, gl: WebGL2RenderingContext) {
+    gl.bindFramebuffer(gl.FRAMEBUFFER, target.fbo);
+    const length = target.width * target.height * 4;
+    const texture = new Float32Array(length);
+    gl.readPixels(0, 0, target.width, target.height, gl.RGBA, gl.FLOAT, texture);
+    return texture;
+  }
+
+  public static textureToCanvas(texture: Uint8Array, width: number, height: number) {
+    const captureCanvas = document.createElement("canvas");
+    const ctx = captureCanvas.getContext("2d")!;
+    captureCanvas.width = width;
+    captureCanvas.height = height;
+
+    const imageData = ctx.createImageData(width, height);
+    imageData.data.set(texture);
+    ctx.putImageData(imageData, 0, 0);
+
+    return captureCanvas;
+  }
+
+  public static invertImageColors(canvas: HTMLCanvasElement) {
+    const ctx = canvas.getContext("2d")!;
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      imageData.data[i] = 255 - imageData.data[i]!;
+      imageData.data[i + 1] = 255 - imageData.data[i + 1]!;
+      imageData.data[i + 2] = 255 - imageData.data[i + 2]!;
+    }
+    ctx.putImageData(imageData, 0, 0);
+  }
+
+  public static downloadURI(filename: string, uri: string) {
+    const link = document.createElement("a");
+    link.download = filename;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+}
+
+export { Screenshot };
